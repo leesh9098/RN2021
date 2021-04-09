08672f07e0a8f376de30eab649488161e721067a
leesh9098 <leesh3578@gmail.com>
2021-04-09 15:01:06 +0800

    21.04.09\

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  */
 
 import React from 'react'; // 추가
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './app/App'; // 수정
-    const TodoApp = () => <App /> // 추가
-import {name as appName} from './app.json';
+const TodoApp = () => <App /> // 추가
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => TodoApp); // 수정
