c86af0875819bbaa2df7ab41cf15db084232d8b1
leesh9098 <77374838+leesh9098@users.noreply.github.com>
2021-05-14 14:54:54 +0800

    21.05.14

--- ch4/App.js
+++ ch4/App.js
@@ -1,13 +1,16 @@
-// 코드 4.7
+import React, { Component } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 
-import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native';
-
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.cardContainer} />
+                <View style={styles.cardContainer}>
+                    <View style={styles.cardImageContainer}>
+                        <Image style={styles.cardImage}
+                            source={require('./user.png')} />
+                    </View>
+                </View>
             </View>
         );
     }
@@ -22,10 +25,28 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     cardContainer: {
+        alignItems: 'center', // 4.14 수정
+        borderColor: 'black',
+        borderWidth: 3,
+        borderStyle: 'solid', // 4.14 수정
+        borderRadius: 20,
         backgroundColor: profileCardColor,
         width: 300,
         height: 400
+    },
+    cardImageContainer: {
+        alignItems: 'center', // 4.14 수정
+        backgroundColor: 'white',
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginTop: 30, // 4.14 수정
+        paddingTop: 15 // 4.14 수정
+    },
+    cardImage: {
+        width: 80,
+        height: 80
     }
 });
-
-export default App;
