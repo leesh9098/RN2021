8f8370a17813df42322b1821109fcbfdb7adb412
leesh9098 <77374838+leesh9098@users.noreply.github.com>
2021-06-04 22:04:01 +0800

    21.06.04

---
+++ ch8/App_8.1.js
@@ -0,0 +1,61 @@
+import React, { createContext, Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ThemeContext = createContext()
+
+
+class Parent extends Component {
+    state = { themeValue: 'light' }
+    toggleThemeValue = () => {
+        const value = this.state.themeValue === 'dark' ? 'light' : 'dark'
+        this.setState({ themeValue: value })
+    }
+    render() {
+        return (
+            <ThemeContext.Provider
+                value={{
+                    themeValue: this.state.themeValue,
+                    toggleThemeValue: this.toggleThemeValue
+                }}
+            >
+                <View style={styles.container}>
+                    <Text>Hello World</Text>
+                </View>
+                <Child1 />
+            </ThemeContext.Provider>
+        );
+    }
+}
+
+const Child1 = () => <Child2 />
+
+const Child2 = () => (
+    <ThemeContext.Consumer>
+        {(val) => (
+            <View style={[styles.container,
+            val.themeValue === 'dark' &&
+            { backgroundColor: 'black' }]}>
+                <Text style={styles.text}>Hello from Component2</Text>
+                <Text style={styles.text}
+                    onPress={val.toggleThemeValue}>
+                    Toggle Theme Value
+          </Text>
+            </View>
+        )}
+    </ThemeContext.Consumer>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        fontSize: 22,
+        color: '#666'
+    }
+})
+
+export default Parent;
